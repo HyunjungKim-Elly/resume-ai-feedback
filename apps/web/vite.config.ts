9b194b2a0afee8a934bae3f048917b3c0ded09d0
Hyunjung Kim <ujujo@naver.com>
2025-07-15 00:55:12 +0900

    모노레포 구조로 변경

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  root: './', // 루트는 기본값 그대로
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), 
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
